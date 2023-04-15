0bc8efd3e1acc58790518e7e995f2baaa53b5664
Or Sharabi <hpgresh@gmail.com>
2023-04-15 21:12:51 +0300

    Render HTML using context and custom tags.

--- src/ecommerceEBR/urls.py
+++ src/ecommerceEBR/urls.py
@@ -18,6 +18,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home_page),
+    path('', views.home_page),
+    path('about/', views.about_page),
+    path('contact/', views.contact_page),
     path('admin/', admin.site.urls),
 ]
