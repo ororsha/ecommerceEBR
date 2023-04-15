bad8b631938e2c88bd7235600af3b0d4d182f515
Or Sharabi <hpgresh@gmail.com>
2023-04-15 20:16:47 +0300

    Render HTML test

--- src/ecommerceEBR/urls.py
+++ src/ecommerceEBR/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('home/', views.home_page),
     path('admin/', admin.site.urls),
 ]
