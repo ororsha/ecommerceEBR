cf6f431cfc11600f266d3d02800cd7063bb3f6c6
Or Sharabi <hpgresh@gmail.com>
2023-04-30 01:30:14 +0300

    Custom user manager

--- src/ecommerceEBR/settings.py
+++ src/ecommerceEBR/settings.py
@@ -51,6 +51,8 @@ INSTALLED_APPS = [
     'orders'
 ]
 
+AUTH_USER_MODEL = 'accounts.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
