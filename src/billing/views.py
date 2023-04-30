0b2def46ee4d197b8f4652e7b1b6520d5cf9cd7b
Or Sharabi <hpgresh@gmail.com>
2023-05-01 00:25:06 +0300

    Stripe integration

--- src/billing/views.py
+++ src/billing/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import render, redirect
+from django.utils.http import url_has_allowed_host_and_scheme
 
-# Create your views here.
+import stripe
+STRIPE_SECRET_KEY = getattr(settings, "STRIPE_SECRET_KEY", "sk_test_51IZM9RIfI9UYOkDFDD1kcINyk5Dvm4O4PHKebKSfkxC8avfMK9Q1qcadymdw6w0UJSZO5EFHtuYpFO9QOMI3sVzw00m2vY0RjR")
+STRIPE_PUB_KEY =  getattr(settings, "STRIPE_PUB_KEY", 'pk_test_51IZM9RIfI9UYOkDF7FsVucjdcFkIh4DPqJmb5x7JWlmRM5xG8vChbfuH8FOZsEyH13avwdpGZqalCwORzuSxA3Qd0073zMYQRV')
+stripe.api_key = STRIPE_SECRET_KEY
+
+
+from .models import BillingProfile, Card
+
+
+def payment_method_view(request):
+    #next_url =
+    # if request.user.is_authenticated():
+    #     billing_profile = request.user.billingprofile
+    #     my_customer_id = billing_profile.customer_id
+
+    billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+    if not billing_profile:
+        return redirect("/cart")
+    next_url = None
+    next_ = request.GET.get('next')
+    if url_has_allowed_host_and_scheme(next_, request.get_host()):
+        next_url = next_
+    return render(request, 'billing/payment-method.html', {"publish_key": STRIPE_PUB_KEY, "next_url": next_url})
+
+
+def payment_method_createview(request):
+    if request.method == "POST" and is_ajax(request=request):
+        billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+        if not billing_profile:
+            return HttpResponse({"message": "Cannot find this user"}, status_code=401)
+        token = request.POST.get("token")
+        if token is not None:
+            new_card_obj = Card.objects.add_new(billing_profile, token)
+            print(new_card_obj)  # start saving our cards too!
+        return JsonResponse({"message": "Success! Your card was added."})
+    return HttpResponse("error", status_code=401)
+
+
+def is_ajax(request):
+    return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
