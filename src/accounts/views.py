5984a957311946de03ceba5965585300f3a91d6f
Or Sharabi <hpgresh@gmail.com>
2023-05-02 01:22:45 +0300

    User accounts

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -4,15 +4,16 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.urls import reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, FormView, DetailView, View
+from django.views.generic import CreateView, FormView, DetailView, View, UpdateView
+from django.views.generic.edit import FormMixin
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
 from django.utils.http import url_has_allowed_host_and_scheme
 from django.utils.safestring import mark_safe
-from .models import EmailActivation
 from .signals import user_logged_in
 
-from .forms import LoginForm, RegisterForm
+from .forms import LoginForm, RegisterForm, ReactivateEmailForm, UserDetailChangeForm
+from .models import EmailActivation
 
 
 # @login_required # /accounts/login/?next=/some/path/
@@ -27,10 +28,62 @@ class AccountHomeView(LoginRequiredMixin, DetailView):
         return self.request.user
 
 
+class AccountEmailActivateView(FormMixin, View):
+    success_url = '/login/'
+    form_class = ReactivateEmailForm
+    key = None
+    def get(self, request, key=None, *args, **kwargs):
+        self.key = key
+        if key is not None:
+            qs = EmailActivation.objects.filter(key__iexact=key)
+            confirm_qs = qs.confirmable()
+            if confirm_qs.count() == 1:
+                obj = confirm_qs.first()
+                obj.activate()
+                messages.success(request, "Your email has been confirmed. Please login.")
+                return redirect("login")
+            else:
+                activated_qs = qs.filter(activated=True)
+                if activated_qs.exists():
+                    reset_link = reverse("password_reset")
+                    msg = """Your email has already been confirmed
+                    Do you need to <a href="{link}">reset your password</a>?
+                    """.format(link=reset_link)
+                    messages.success(request, mark_safe(msg))
+                    return redirect("login")
+        context = {'form': self.get_form(),'key': key}
+        return render(request, 'registration/activation-error.html', context)
+
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        msg = """Activation link sent, please check your email."""
+        request = self.request
+        messages.success(request, msg)
+        email = form.cleaned_data.get("email")
+        obj = EmailActivation.objects.email_exists(email).first()
+        user = obj.user
+        new_activation = EmailActivation.objects.create(user=user, email=email)
+        new_activation.send_activation()
+        return super(AccountEmailActivateView, self).form_valid(form)
+
+    def form_invalid(self, form):
+        context = {'form': form, "key": self.key}
+        return render(self.request, 'registration/activation-error.html', context)
+
+
 class LoginView(FormView):
     form_class = LoginForm
     success_url = '/'
     template_name = 'accounts/login.html'
+
+    def form_invalid(self, form):
+        return super(LoginView, self).form_invalid(form)
 
     def form_valid(self, form):
         request = self.request
@@ -40,8 +93,10 @@ class LoginView(FormView):
         email  = form.cleaned_data.get("email")
         password  = form.cleaned_data.get("password")
         user = authenticate(request, username=email, password=password)
+        print(user)
         if user is not None:
             if not user.is_active:
+                print('inactive user..')
                 messages.error(request, "This user is inactive")
                 return super(LoginView, self).form_invalid(form)
             login(request, user)
@@ -53,35 +108,27 @@ class LoginView(FormView):
         return super(LoginView, self).form_invalid(form)
 
 
-class AccountEmailActivateView(View):
-    def get(self, request, key, *args, **kwargs):
-        qs = EmailActivation.objects.filter(key__iexact=key)
-        confirm_qs = qs.confirmable()
-        if confirm_qs.count() == 1:
-            obj = confirm_qs.first()
-            obj.activate()
-            messages.success(request, "Your email has been confirmed. Please login.")
-            return redirect("login")
-        else:
-            activated_qs = qs.filter(activated=True)
-            if activated_qs.exists():
-                reset_link = reverse("password_reset")
-                msg = """Your email has already been confirmed
-                Do you need to <a href="{link}">reset your password</a>?
-                """.format(link=reset_link)
-                messages.success(request, mark_safe(msg))
-                return redirect("login")
-        return render(request, 'registration/activation-error.html', {})
-
-    def post(self, request, *args, **kwargs):
-        # create form to receive an email
-        pass
-
-
 class RegisterView(CreateView):
     form_class = RegisterForm
     template_name = 'accounts/register.html'
     success_url = '/login/'
+
+
+class UserDetailUpdateView(LoginRequiredMixin, UpdateView):
+    form_class = UserDetailChangeForm
+    template_name = 'accounts/detail-update-view.html'
+
+    def get_object(self):
+        return self.request.user
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(UserDetailUpdateView, self).get_context_data(*args, **kwargs)
+        context['title'] = 'Change Your Account Details'
+        return context
+
+    def get_success_url(self):
+        return reverse("account:home")
+
 
 
 # def login_page(request):
